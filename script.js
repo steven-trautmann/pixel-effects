0b60a01ffdf0f43c62d0889c85290ff77e2139c0
Trautmann István <istvan.trautmann@frontside.hu>
2021-03-28 18:54:12 +0200

    rainy effect with different speed

--- script.js
+++ script.js
@@ -25,19 +25,51 @@ const greyscale = () => {
 
 const rainyAnimation = () => {
     ctx.drawImage(image1, 0, 0, canvas.width, canvas.height);
+    const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
     let particlesArray = [];
     const numberOfParticles = 5000;
+
+    let mappedImage = [];
+    for (let y = 0; y < canvas.height; y++) {
+        let row = [];
+        for (let x = 0; x < canvas.width; x++) {
+            const red = pixels.data[(y * 4 * pixels.width) + (x * 4)];
+            const green = pixels.data[(y * 4 * pixels.width) + (x * 4 + 1)];
+            const blue = pixels.data[(y * 4 * pixels.width) + (x * 4 + 2)];
+            const brightness = calculateRelativeBrightness(red, green, blue);
+            const cell = [
+                cellBrightness = brightness
+            ];
+            row.push(cell);
+        }
+        mappedImage.push(row);
+    }
+    console.log(mappedImage);
+
+    function calculateRelativeBrightness(red, green, blue) {
+        return Math.sqrt(
+            (red * red) * 0.299 +
+            (green * green) * 0.587 +
+            (blue * blue) * 0.114
+        )/100;
+    }
 
     class Particle {
         constructor() {
             this.x = Math.random() * canvas.width;
             this.y = 0;
             this.speed = 0;
-            this.velocity = Math.random() * 3.5;
+            this.velocity = Math.random() * 0.5;
             this.size = Math.random() * 1.5 + 1;
         }
         update() {
-            this.y += this.velocity;
+            this.position1 = Math.floor(this.y);
+            this.position2 = Math.floor(this.x);
+            this.speed = mappedImage[this.position1][this.position2][0];
+            let movement = this.speed + this.velocity;
+
+            this.y += movement;
             if (this.y >= canvas.height){
                 this.y = 0;
                 this.x = Math.random() * canvas.width;
